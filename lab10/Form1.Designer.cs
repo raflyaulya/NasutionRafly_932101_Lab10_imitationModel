a9978b43b3c0bc4c96907ca6bd205f3d7e78b697
raflyaulya <auliarafli819@gmail.com>
2024-05-14 13:45:10 +0700

    Initial commit

---
+++ lab10/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace lab10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.score = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(541, 411);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 72);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Start !";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(190, 111);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 45);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "France";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(917, 111);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(257, 45);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "United Kingdom";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 505);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 0;
+            // 
+            // score
+            // 
+            this.score.AutoSize = true;
+            this.score.Font = new System.Drawing.Font("SimSun", 50F);
+            this.score.Location = new System.Drawing.Point(527, 232);
+            this.score.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(245, 84);
+            this.score.TabIndex = 8;
+            this.score.Text = "0 - 0";
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = global::lab10.Properties.Resources.f_rance_2;
+            this.panel2.Location = new System.Drawing.Point(96, 179);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(317, 175);
+            this.panel2.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::lab10.Properties.Resources.british_copy;
+            this.panel1.Location = new System.Drawing.Point(891, 179);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(328, 190);
+            this.panel1.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(474, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(368, 57);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "FIFA World Cup ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1280, 592);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.score);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label score;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ lab10/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            score.Text = $"{GetScore(5)} - {GetScore(1)}";
+        }
+
+        private int GetScore(double lambda)
+        {
+            double S = 0; // Инициализируем переменную S для хранения суммы
+            int m = 0;  // Инициализируем переменную m для подсчета итераций
+
+            Random random = new Random(); // содаем для генерации случайных чисел
+
+            while(S>= -lambda) // Запускаем цикл, который продолжается, пока S больше или равно -lambda
+            {
+                S += Math.Log(random.NextDouble()); // Генерируем случайное вещественное число между 0.0 и 1.0 и добавляем его натуральный логарифм к S
+                m++; // увеличаем счетных итерации
+            }
+            return m;
+        }
+    }
+}
